afc3637531852e168ae8cc9186e6b02f80eca491
Freemano <freemano@Freeman.local>
2024-05-01 10:24:16 +0300

    added rest api

---
+++ web-service-gin/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+    "net/http"
+    "os"
+    "github.com/gin-gonic/gin"
+    "github.com/joho/godotenv"
+)
+// album represents data about a record album.
+type album struct {
+    ID     string  `json:"id"`
+    Title  string  `json:"title"`
+    Artist string  `json:"artist"`
+    Price  float64 `json:"price"`
+}
+// albums slice to seed record album data.
+var albums = []album{
+    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, albums)
+}
+var env = make(map[string]string)
+
+func init() {
+    // Load variables from .env file into the environment
+    err := godotenv.Load(".env")
+    if err != nil {
+        // Handle error if .env file cannot be loaded
+        panic("Error loading .env file")
+    }
+
+    // Populate env map with environment variables
+    env["home"] = os.Getenv("HOME")
+    env["user"] = os.Getenv("USER")
+    env["api_key"] = os.Getenv("API_KEY")
+}
+
+func showEnv(c *gin.Context) {
+    // Send env variable as JSON response with indentation
+    c.IndentedJSON(http.StatusOK, env)
+}
+
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+    var newAlbum album
+
+    // Call BindJSON to bind the received JSON to
+    // newAlbum.
+    if err := c.BindJSON(&newAlbum); err != nil {
+        return
+    }
+
+    // Add the new album to the slice.
+    albums = append(albums, newAlbum)
+    c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+    id := c.Param("id")
+
+    // Loop over the list of albums, looking for
+    // an album whose ID value matches the parameter.
+    for _, a := range albums {
+        if a.ID == id {
+            c.IndentedJSON(http.StatusOK, a)
+            return
+        }
+    }
+    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+func main() {
+    router := gin.Default()
+    router.GET("/env", showEnv)
+    router.GET("/albums", getAlbums)
+    router.GET("/albums/:id", getAlbumByID)
+    router.POST("/albums", postAlbums)
+
+    router.Run("localhost:8080")
+}
